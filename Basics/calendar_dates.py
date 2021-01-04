616cb4c7418bdd5b3353b2ab229b2001dd2b0f9e
Farid Karimli <karimli.farid@icloud.com>
2021-01-04 17:17:44 +0400

    Update and rename ps8pr2.py to calendar_dates.py

--- Basics/calendar_dates.py
+++ Basics/calendar_dates.py
@@ -1,10 +1,8 @@
 #
-# ps8pr2.py  (Problem Set 8, Problem 2)
-#
-# A class to represent calendar dates       
 #
 
 class Date:
+    
     """ A class that stores and manipulates dates that are
         represented by a day, month, and year.
     """
